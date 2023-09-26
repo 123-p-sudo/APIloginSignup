cdcced35c0ebec747e5d87d1eb89e90ded53de70
Priya <singhpiu57@gmail.com>
2023-09-26 18:30:56 +0530

    APIcode

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path,include
+from account.views import  UserProfileView, UserRegisterationView,UserLoginView
+from . import views
+urlpatterns = [
+    path('register/',UserRegisterationView.as_view(),name='register'),
+    path('login/',UserLoginView.as_view(),name='login'),
+    path('profile/',UserProfileView.as_view(),name='profile'),
+    path('register1/',views.registration,name='register1'), 
+    path('login1/',views.login1,name='login1'),   
+  
+]
---
+++ account/views.py
@@ -0,0 +1,123 @@
+from logging import raiseExceptions
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from account.serializers import  UserRegisterationSerializer,UserLoginSerializer
+from django.contrib.auth import authenticate
+from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from account.serializers import UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes,permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+
+
+#generate token manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+
+class UserRegisterationView(APIView):
+        
+  renderer_classes = [UserRenderer]
+
+  def post(self,request,format=None):
+            serializer = UserRegisterationSerializer(data=request.POST)
+            if serializer.is_valid(raise_exception=True):
+                  user = serializer.save()
+                  user.set_password(request.data['password'])
+                  user.save
+                  #token = get_tokens_for_user(user)
+                  #access_token = token['access']
+                  #refresh_token = token['refresh']
+                  #print("access_token:" ,access_token)
+                  #print("refresh_token:",refresh_token)
+                  #return Response({'msg':'Registeration Success','token':token},status=status.HTTP_201_CREATED)
+                  token = Token.objects.create(user=user)
+                  print(token)
+                  return Response({'msg':'Registeration Success',"token":token.key},status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+            #return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+  def  get(self, request, format=None):
+        # Render the signup.html template for GET requests
+        return render(request, 'signup.html')
+  
+def registration(request):
+    return render(request, 'signup.html')
+    
+
+def login1(request):
+    return render(request, 'login.html')
+
+
+class UserLoginView(APIView):
+      renderer_classes = [UserRenderer]
+      def post(self,request,format=None):
+             serializer = UserLoginSerializer(data = request.POST)
+             if serializer.is_valid(raise_exception=True):
+                   email = serializer.data.get('email')
+                   password = serializer.data.get('password')
+                   user = authenticate(email = email,password=password)
+                   if user is not None:
+                         token,created = Token.objects.get_or_create(user=user)
+                         #refresh_token_login=token['refresh']
+                         #access_token_login=token['access']
+                         #print("access_token:" ,access_token_login)
+                         #print("refresh_token:",refresh_token_login)
+                         print(token)
+
+                         return Response({'msg':'Login Success','token':token.key},status=status.HTTP_200_OK)
+                   else:
+                          return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}},status=status.HTTP_404_NOT_FOUND)
+                   
+
+             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+      
+      def  get(self, request, format=None):
+        # Render the signup.html template for GET requests
+        return render(request, 'login.html')
+
+
+
+class UserProfileView(APIView):
+      renderer_classes=[UserRenderer]
+      permission_classes=[IsAuthenticated]
+      authentication_classes=([SessionAuthentication,TokenAuthentication])
+      def get(self,request,format=None):
+            serializer = UserProfileSerializer(request.user) 
+            return Response(serializer.data, status=status.HTTP_200_OK)
+      
+
+
+      
+
+
+
+
+
+
+      
+
+
+
+
+             
+
+
+
+
+                         
+        
+
+
